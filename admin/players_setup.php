e3f22ab3999023646d87743d6fdbc8b1625d8972
atm-lena <lena@atm-consulting.fr>
2020-04-15 12:03:44 +0200

    Add a player - in progress

--- admin/players_setup.php
+++ admin/players_setup.php
@@ -30,6 +30,8 @@ if (! $res) {
 // Libraries
 require_once DOL_DOCUMENT_ROOT . '/core/lib/admin.lib.php';
 require_once '../lib/doligame.lib.php';
+dol_include_once('abricot/includes/lib/admin.lib.php');
+
 
 // Translations
 $langs->load('doligame@doligame');
@@ -37,6 +39,40 @@ $langs->load('doligame@doligame');
 // Access control
 if (! $user->admin) {
     accessforbidden();
+}
+
+// Parameters
+$action = GETPOST('action', 'alpha');
+
+/*
+ * Actions
+ */
+if (preg_match('/set_(.*)/', $action, $reg))
+{
+    $code=$reg[1];
+    if (dolibarr_set_const($db, $code, GETPOST($code), 'chaine', 0, '', $conf->entity) > 0)
+    {
+        header("Location: ".$_SERVER["PHP_SELF"]);
+        exit;
+    }
+    else
+    {
+        dol_print_error($db);
+    }
+}
+
+if (preg_match('/del_(.*)/', $action, $reg))
+{
+    $code=$reg[1];
+    if (dolibarr_del_const($db, $code, 0) > 0)
+    {
+        Header("Location: ".$_SERVER["PHP_SELF"]);
+        exit;
+    }
+    else
+    {
+        dol_print_error($db);
+    }
 }
 
 /*
@@ -55,7 +91,55 @@ $head = doligameAdminPrepareHead();
 dol_fiche_head(
     $head,
     'players_setup',
-    $langs->trans('Module104662Name'),
-    0,
-    'doligame@doligame'
+    $langs->trans("Module104662Name"),
+    -1,
+    "doligame@doligame"
 );
+
+
+// Setup page goes here
+$form=new Form($db);
+$var=false;
+print '<table class="noborder" width="100%">';
+
+
+if(!function_exists('setup_print_title')){
+    print '<div class="error" >'.$langs->trans('AbricotNeedUpdate').' : <a href="http://wiki.atm-consulting.fr/index.php/Accueil#Abricot" target="_blank"><i class="fa fa-info"></i> Wiki</a></div>';
+    exit;
+}
+
+setup_print_title("AddPlayers");
+
+//Liste d'utilisateurs à ajouter
+
+//TODO : exclure les utilisateurs déjà enregistrés en tant que joueur
+$TUsersExclude = array();
+
+print '<form action="'.$_SERVER["PHP_SELF"].'" method="post">';
+print '<input type="hidden" name="action" value="add_player">';
+
+print '<table class="noborder centpercent">';
+
+print '<tr class="oddeven">';
+print '<td>';
+print '<label>'.$langs->trans("Users").'</label>';
+print $form->select_users('', '', '', $TUsersExclude);
+print '</td>';
+print '</tr>';
+
+print '</table>';
+
+print '<div class="center">';
+print '<input type="submit" class="button" value="' . $langs->trans("AddPlayer") . '" name="button">';
+print '</div>';
+
+print '</form>';
+
+
+print '</table>';
+
+dol_fiche_end(-1);
+
+llxFooter();
+
+$db->close();
